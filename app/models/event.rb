74ba9de9e54b7972e4f641d188aec7214167ab5f
Gina Buchholz <glb722@gmail.com>
2014-02-25 15:11:38 -0600

    Added tests to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
 class Event < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: { minimum: 1}
+  validates :name, length: { maximum: 256}
+  
+  validates :date, presence: true
+  
+  validates :description, presence: true
+  validates :description, length: { minimum: 1}
+  validates :description, length: { maximum: 256 }
+  
+  validates :location, presence: true
+  validates :location, length: { minimum: 1}
+  validates :location, length: { maximum: 256}
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should always have name" do
+    event = Event.new
+	event.name = ""
+	assert !event.save, "Saved the event without a name" 
+  end
+  
+  test "should always have name less than 256 characters" do
+    event = Event.new
+	event.name = "a"*256
+	assert !event.save, "Saved the event with a name more than 256 characters"
+  end
+  
+  test "should always have date" do
+    event = Event.new
+	event.date = nil
+	assert !event.save, "Saved the event without a date"
+  end
+  
+  test "should always have content in description" do
+    event = Event.new
+	event.description = ""
+	assert !event.save, "Saved the event without the description"
+  end
+  
+  test "should have description less than 256 characters" do
+    event = Event.new
+	event.description = "a"*256
+	assert !event.save, "Saved the event with a description with more than 256 characters"
+  end
+  
+  test "should always have content in location" do
+    event = Event.new
+	event.location = ""
+	assert !event.save, "Saved the event without the location"
+  end
+  
+  test "should have location less than 256 characters" do
+    event = Event.new
+	event.location = "a"*256
+	assert !event.save, "Saved the event with a location with more than 256 characters"
+  end
+  
 end
